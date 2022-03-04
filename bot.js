c3e663d4d9866fa8e572d911789b31c3e9899757
FlishCake@ya.ru <FlishCake@ya.ru>
2022-03-04 07:26:43 +0300

    test

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
-const { Telegraf } = require('telegraf')
+const { Telegraf, Markup, session } = require('telegraf')
 require("dotenv").config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.start((ctx) => ctx.reply("Привет, Друже!\nНапиши мне своё имя и фамилию.\n(Пример: Иван Иванов)"))
+bot.start((ctx) => ctx.reply(’Привет, ${ctx.session.name}!\nНапиши мне своё имя и фамилию.\n(Пример: Иван Иванов)’))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('message', (ctx) => ctx.reply("Упс. Что-то пошло не так ;)"))
 
 bot.launch()
 
