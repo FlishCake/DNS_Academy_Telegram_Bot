4d4d423097577ebc5f1cec4b9230a6c05034a18e
FlishCake@ya.ru <FlishCake@ya.ru>
2022-03-15 23:14:11 +0300

    gix

--- dbUsers/index.js
+++ dbUsers/index.js
@@ -10,13 +10,13 @@ db.serialize(() => {
   db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, telegram_id INTEGER, firstname VARCHAR(40), lastname VARCHAR(40), user_group VARCHAR(40));');
 
   //db.run('INSERT INTO users (telegram_id, firstname, lastname, user_group) VALUES (111111, "Валентин", "Назаров", "Admin");')
-});
+
 
 let getUser = id => {
   let user = {};
   
   //db.run(`INSERT INTO users (telegram_id, firstname, lastname, user_group) VALUES (${id}, "Валентин", "Назаров", "guest");`)
-  db.serialize(() => {
+  
   db.each(`SELECT * FROM users WHERE telegram_id = ${id};`, (err, row) => {
     if (err) {
       console.error(err.message);
@@ -24,13 +24,13 @@ let getUser = id => {
     console.log(row);
     Object.assign(user, row);
   });
-  });
+  
   
   console.log(user);
 }
 
 module.exports.getUser = getUser;
-
+});
 /*
 db.close((err) => {
   if (err) {
