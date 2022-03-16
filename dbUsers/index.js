8d0283666659779a698459b9c3b442e26f06ddc9
FlishCake@ya.ru <FlishCake@ya.ru>
2022-03-16 07:55:44 +0300

    fix

--- dbUsers/index.js
+++ dbUsers/index.js
@@ -1,8 +1,8 @@
 var sqlite3 = require('sqlite3').verbose();
 
+let user = {};
 
 let getUser = id => {
-  let user = null;
   
   let db = new sqlite3.Database('./users.db', (err) => {
     if (err) {
@@ -14,8 +14,6 @@ let getUser = id => {
   db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, telegram_id INTEGER, firstname VARCHAR(40), lastname VARCHAR(40), user_group VARCHAR(40));');
 
   //db.run('INSERT INTO users (telegram_id, firstname, lastname, user_group) VALUES (111111, "Валентин", "Назаров", "Admin");')
-
-  //let user = {};
   
   //db.run(`INSERT INTO users (telegram_id, firstname, lastname, user_group) VALUES (${id}, "Валентин", "Назаров", "guest");`)
   
