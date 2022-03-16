bf73b2e77c6b491b191bc15f8c5c8c20733a2b62
FlishCake@ya.ru <FlishCake@ya.ru>
2022-03-16 07:49:18 +0300

    fix

--- dbUsers/index.js
+++ dbUsers/index.js
@@ -2,19 +2,19 @@ var sqlite3 = require('sqlite3').verbose();
 
 
 let getUser = id => {
-var db = new sqlite3.Database('./users.db', (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to the in-memory SQlite database.');
-});
+  let user = null;
+  
+  let db = new sqlite3.Database('./users.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+  });
 
-db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, telegram_id INTEGER, firstname VARCHAR(40), lastname VARCHAR(40), user_group VARCHAR(40));');
+  db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, telegram_id INTEGER, firstname VARCHAR(40), lastname VARCHAR(40), user_group VARCHAR(40));');
 
   //db.run('INSERT INTO users (telegram_id, firstname, lastname, user_group) VALUES (111111, "Валентин", "Назаров", "Admin");')
 
-
-let id => {
   //let user = {};
   
   //db.run(`INSERT INTO users (telegram_id, firstname, lastname, user_group) VALUES (${id}, "Валентин", "Назаров", "guest");`)
@@ -23,17 +23,20 @@ let id => {
     if (err) {
       console.error(err.message);
     }
-    return(row);
+    Object.assign(row, user);
   });
-};}
+  
+  db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+    return user;
+  });
+}
 
 module.exports.getUser = getUser;
 
 /*
-db.close((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
+
 */
